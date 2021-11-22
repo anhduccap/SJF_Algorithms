a0076b0b7a81893413d8e258a6843b33f6af0bdc
Cáp Hữu Anh Đức <59206083+anhduccap@users.noreply.github.com>
2021-11-22 23:46:58 +0700

    Create testcase.js

---
+++ testcase.js
@@ -0,0 +1,106 @@
+// Các process đến cùng lúc
+exports.case1 = [
+    {
+        process_name: 1,
+        arrivalTime: 0,
+        burstTime: 6,
+        start: null,
+        end: null,
+    },
+    {
+        process_name: 2,
+        arrivalTime: 0,
+        burstTime: 2,
+        start: null,
+        end: null,
+    },
+    {
+        process_name: 3,
+        arrivalTime: 0,
+        burstTime: 8,
+        start: null,
+        end: null,
+    },
+    {
+        process_name: 4,
+        arrivalTime: 0,
+        burstTime: 3,
+        start: null,
+        end: null,
+    },
+    {
+        process_name: 5,
+        arrivalTime: 0,
+        burstTime: 4,
+        start: null,
+        end: null,
+    },
+];
+
+//Mỗi process đến vào mỗi thời điểm khác nhau
+exports.case2 = [
+    {
+        process_name: 1,
+        arrivalTime: 0,
+        burstTime: 6,
+        start: null,
+        end: null,
+    },
+    {
+        process_name: 2,
+        arrivalTime: 1,
+        burstTime: 2,
+        start: null,
+        end: null,
+    },
+    {
+        process_name: 3,
+        arrivalTime: 2,
+        burstTime: 8,
+        start: null,
+        end: null,
+    },
+    {
+        process_name: 4,
+        arrivalTime: 3,
+        burstTime: 3,
+        start: null,
+        end: null,
+    },
+    {
+        process_name: 5,
+        arrivalTime: 4,
+        burstTime: 4,
+        start: null,
+        end: null,
+    },
+];
+
+// Có 2 process đến cùng lúc
+exports.case3 = [
+    {
+        process_name: 1,
+        arrivalTime: 0,
+        burstTime: 2,
+    },
+    {
+        process_name: 2,
+        arrivalTime: 1,
+        burstTime: 2,
+    },
+    {
+        process_name: 3,
+        arrivalTime: 2,
+        burstTime: 8,
+    },
+    {
+        process_name: 4,
+        arrivalTime: 2,
+        burstTime: 3,
+    },
+    {
+        process_name: 5,
+        arrivalTime: 3,
+        burstTime: 3,
+    },
+];
